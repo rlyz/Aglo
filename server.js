5c3f02bc64b91222bd5d6f868bdd92c94575b983
rlyz <rayleeyz@gmail.com>
2020-10-16 10:15:06 +0800

    testing express config

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ app.use(express.urlencoded({
 }));
 
 const clientBuildPath = path.join(__dirname, '/client/build')
-app.use(express.static(clientBuildPath))
-
-
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client/build', 'index.html')));
+}
 // DB config
 const uri = process.env.mongoURI
 const connection = mongoose.connection;
